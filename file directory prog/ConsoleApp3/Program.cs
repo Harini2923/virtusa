9df4f85b47bf7fd930fc6cda0ef2bce66310e12a
Harini2923 <75440804+Harini2923@users.noreply.github.com>
2022-11-22 22:25:28 +0530

    Add files via upload

---
+++ file directory prog/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class dir
+{
+    public static void Main(string[] arg)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        String [] a = new String[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i] = Console.ReadLine();
+        }
+        Stack<String> s = new Stack<String>();
+
+        for (int i = 0; i < n; i++)
+        {
+
+            if (a[i] == "../" && s.Count != 0)
+            {
+
+                s.Pop();
+            }
+
+
+            else if (a[i] != "./")
+            {
+
+
+                s.Push(a[i]);
+            }
+        }
+        int k = s.Count();
+        Console.WriteLine(k);
+
+    }
+}
